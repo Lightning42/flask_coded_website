f9a52a9981fdf6ae91d57d9ab34114fbbcfd3ef9
Lightning42 <50424675+Lightning42@users.noreply.github.com>
2019-11-10 22:04:53 +0000

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,12 @@
-from flask import Flask
-from flask import render_template
-from flask import url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '24944d6d0064003ab376ed5a113cebc8'
+
 
 posts = [
     {
@@ -39,7 +43,7 @@ posts = [
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -48,11 +52,27 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/login")
-def login():
-    return render_template('login.html', title='Login')
-
-
 @app.route("/test")
 def test():
     return render_template('test.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
