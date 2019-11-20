489247df6166c6de0e56bbdccf3addb81f597ec3
Lightning42 <50424675+Lightning42@users.noreply.github.com>
2019-11-20 20:55:31 +0000

    Update forms.py
    Added username and email duplicate verification

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken.')
 
 
 class LoginForm(FlaskForm):
