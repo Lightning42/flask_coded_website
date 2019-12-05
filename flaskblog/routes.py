f030ba0bf0d99226326e52318980b057e47aab88
Lightning42 <50424675+Lightning42@users.noreply.github.com>
2019-12-05 18:17:07 +0000

    Update routes.py
    Imported Pillow. Fixed syntax errors and added resize function to profile pictures to save space.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -96,9 +97,13 @@ def logout():
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex = f_ext
+    picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (150, 150)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
